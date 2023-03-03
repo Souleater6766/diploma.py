4491fdc616eb84dab8f734fcb9c13d5a5653a930
Jose A Diaz <88800637+Souleater6766@users.noreply.github.com>
2023-03-03 09:38:56 -0600

    Add files via upload

---
+++ diploma.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Certificate of Completion</title>
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+		}
+
+		.container {
+			max-width: 600px;
+			margin: auto;
+			padding: 20px;
+			border: 2px solid black;
+		}
+
+		.heading {
+			text-align: center;
+			font-size: 30px;
+			margin-bottom: 30px;
+			text-transform: uppercase;
+		}
+
+		.school {
+			font-size: 20px;
+			font-weight: bold;
+			text-align: center;
+			margin-bottom: 50px;
+		}
+
+		.name {
+			font-size: 25px;
+			font-weight: bold;
+			text-align: center;
+			margin-bottom: 30px;
+			text-transform: uppercase;
+		}
+
+		.text {
+			font-size: 20px;
+			margin-bottom: 20px;
+			text-align: justify;
+			line-height: 1.5;
+		}
+
+		.signature {
+			font-size: 25px;
+			font-weight: bold;
+			margin-top: 50px;
+			text-align: center;
+			text-transform: uppercase;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<div class="heading">Certificate of Completion</div>
+		<div class="school">Boot Camp Python School of Programming</div>
+		<div class="name">José Ángel Díaz Juárez</div>
+		<div class="text">
+			This is to certify that José Ángel Díaz Juárez has successfully completed the CSS course at Boot Camp Python School of Programming. This course has equipped the student with the necessary knowledge and skills to effectively use CSS to create visually appealing and responsive web pages. 
+		</div>
+		<div class="signature">
+			Signature of Director<br>
+			Boot Camp Python School of Programming
+		</div>
+	</div>
+</body>
+</html>
